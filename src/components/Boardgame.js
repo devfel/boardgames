1bf2153115af50f0756120a649df661ac0ba903c
Felizardo <devfelizardo@gmail.com>
2020-09-24 17:07:32 -0300

    initial version of single component for boardgame done

---
+++ src/components/Boardgame.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Boardgame() {
+  return (
+    <div className="boardgame">
+      {" "}
+      <h3>Board Game Title</h3>
+      <p>Board Game Description</p>
+      <p>Rating: 9.0</p>
+    </div>
+  );
+}
+
+export default Boardgame;
