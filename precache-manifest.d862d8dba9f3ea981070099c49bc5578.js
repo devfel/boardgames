10f4c6fbc102f15995b47d6cb9df5e5f7b2e4207
Felizardo <devfelizardo@gmail.com>
2020-09-24 19:34:40 -0300

    Updates

---
+++ precache-manifest.d862d8dba9f3ea981070099c49bc5578.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a283250d6df5d246857203b971de6d4",
+    "url": "/boardgames/index.html"
+  },
+  {
+    "revision": "c3a8023c45d992ea4055",
+    "url": "/boardgames/static/css/main.8fa4d3c8.chunk.css"
+  },
+  {
+    "revision": "40a6eb9fea8a61c3a7e6",
+    "url": "/boardgames/static/js/2.a3d007b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/boardgames/static/js/2.a3d007b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3a8023c45d992ea4055",
+    "url": "/boardgames/static/js/main.2bc62ad1.chunk.js"
+  },
+  {
+    "revision": "ea018af74b05f03c426c",
+    "url": "/boardgames/static/js/runtime-main.23ea5e9a.js"
+  }
+]);
