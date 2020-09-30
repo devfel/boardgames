7a4d5b5fde46d949a2c214c2c159df4929424ab9
Felizardo <devfelizardo@gmail.com>
2020-09-30 01:22:44 -0300

    adding search bar and funtionality

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ const ATLAS_API =
   "https://www.boardgameatlas.com/api/search?&client_id=JLBr5npPhV";
 
 // API to select certain searched itens only (ADD &client_id=JLBr5npPhV)
-const ATLAS_SEARCH_API = "https://www.boardgameatlas.com/api/search?";
+const ATLAS_SEARCH_API = "https://www.boardgameatlas.com/api/search?name=";
 
 const App = () => {
   const [boardgames, setBoardgames] = React.useState(null);
   const [loading, setLoading] = React.useState(true);
+  const [searchTerm, setSearchTerm] = React.useState("");
 
   React.useEffect(() => {
     fetch(ATLAS_API)
@@ -22,15 +23,46 @@ const App = () => {
       });
   }, []);
 
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    fetch(
+      ATLAS_SEARCH_API + searchTerm + "&fuzzy_match=true&client_id=JLBr5npPhV"
+    )
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data.games); // REMOVE THIS AFTER FINISH
+        setBoardgames(data.games);
+        setLoading(false);
+      });
+  };
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
   if (loading === true || boardgames === false)
     return <div className="loading">Loading...</div>;
   else
     return (
-      <div className="boardgames-container">
-        {boardgames.map((item) => (
-          <Boardgame key={item.id} {...item} />
-        ))}
-      </div>
+      <>
+        <header>
+          <form onSubmit={handleOnSubmit}>
+            <input
+              className="search-bar"
+              type="text"
+              placeholder="Search Game..."
+              value={searchTerm}
+              onChange={handleOnChange}
+            />
+          </form>
+        </header>
+        <div className="boardgames-container">
+          {boardgames.map((item) => (
+            <Boardgame key={item.id} {...item} />
+          ))}
+        </div>
+      </>
     );
 };
 export default App;
