500da3b38c535389c3fcbaf9a5c5fdc1ee95f274
Felizardo <devfelizardo@gmail.com>
2020-09-30 02:14:19 -0300

    added some css styling based on the game rating

--- src/components/Boardgame.js
+++ src/components/Boardgame.js
@@ -1,12 +1,24 @@
 import React from "react";
 
 function Boardgame({ name, description_preview, average_user_rating, images }) {
+  const setRatingClass = (rating) => {
+    if (rating >= 80) {
+      return "high";
+    } else if (rating >= 60) {
+      return "medium";
+    } else {
+      return "low";
+    }
+  };
+
   return (
     <div className="boardgame">
       <img src={images.medium} alt={name}></img>
       <div className="boardgame-info">
         <h3>{name}</h3>
-        <span>{(average_user_rating * 20).toFixed(0)}</span>
+        <span className={`tag ${setRatingClass(average_user_rating * 20)}`}>
+          {(average_user_rating * 20).toFixed(0)}
+        </span>
       </div>
 
       <div className="boardgame-desc">
